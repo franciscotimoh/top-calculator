9d7fa1f9b6be0ff86a22136a0b8c959c3aeb4b61
Tim Oh <43765806+franciscotimoh@users.noreply.github.com>
2025-10-20 22:21:40 -0700

    Implement number button event listeners

--- script.js
+++ script.js
@@ -30,3 +30,37 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+let displayText = "";
+
+function updateDisplayText(newText) {
+    const display = document.querySelector('.calculator-display'); 
+    display.textContent = newText; 
+}
+
+numberClasses = {
+    0: ".zero",
+    1: ".one",
+    2: ".two",
+    3: ".three",
+    4: ".four",
+    5: ".five",
+    6: ".six",
+    7: ".seven",
+    8: ".eight",
+    9: ".nine"
+};
+
+for (let i = 0; i < 10; i++) {
+    const numBtn = document.querySelector(numberClasses[i]);
+    numBtn.addEventListener("click", () => {
+        displayText += i.toString();
+        updateDisplayText(displayText); 
+    });
+}
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    displayText = "";
+    updateDisplayText(displayText); 
+})
